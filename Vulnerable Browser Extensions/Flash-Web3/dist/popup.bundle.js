256903792ad8cd67edcccf64cfa0d0bb3873ea64
Abang Obed <66479041+d3vobed@users.noreply.github.com>
2024-04-09 22:46:09 +0100

    Update popup.bundle.js

--- Vulnerable Browser Extensions/Flash-Web3/dist/popup.bundle.js
+++ Vulnerable Browser Extensions/Flash-Web3/dist/popup.bundle.js
@@ -1,24 +1,24 @@
-/******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
-/*!**********************!*\
-  !*** ./src/popup.js ***!
-  \**********************/
+/******The main stuffs**/ 
 document.getElementById('fetchBalance').addEventListener('click', function() {
-    var address = document.getElementById('ethAddress').value;
-    if (address) {
-        getBalance(address);
+    var addresses = document.getElementById('ethAddress').value.split(','); // Split input by comma to handle multiple addresses
+    if (addresses.length > 0) {
+        // Introduce parameter pollution by passing an array of addresses
+        getBalance(addresses);
     } else {
-        document.getElementById('result').innerText = 'Please enter an Ethereum address';
+        document.getElementById('result').innerText = 'Please enter at least one Ethereum address';
     }
 });
 
-function getBalance(address) {
+function getBalance(addresses) {
     var myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
 
+    // Concatenate all addresses into a single string
+    var concatenatedAddresses = addresses.join(',');
+
     var raw = JSON.stringify({
         "method": "eth_getBalance",
-        "params": [address, "latest"],
+        "params": [concatenatedAddresses, "latest"], // Send concatenated addresses
         "id": 1,
         "jsonrpc": "2.0"
     });
@@ -33,10 +33,11 @@ function getBalance(address) {
     fetch("https://quick-quick-model.quiknode.pro/4347eba47aae2cfd9c73ccd76fc8a25cd2bb62bc/", requestOptions)
         .then(response => response.json())
         .then(data => {
+            // Process response as if it were for a single address (not handling multiple addresses properly)
             if (data.result) {
                 var balanceInEther = parseInt(data.result, 16) / 1e18;
                 var balanceFixed = balanceInEther.toFixed(2)
-                document.getElementById('result').innerText = `Balance: ${balanceFixed} ETH`;
+                document.getElementById('result').innerText = `Balance: ${balanceFixed} ETH`; // Display result for a single address (vulnerability)
             } else {
                 document.getElementById('result').innerText = 'Error fetching balance';
             }
@@ -47,6 +48,4 @@ function getBalance(address) {
         });
 }
 
-/******/ })()
-;
 //# sourceMappingURL=popup.bundle.js.map
